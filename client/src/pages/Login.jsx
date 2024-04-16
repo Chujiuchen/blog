19059f38d2593050a3e7a90653b0536995e28e9c
chujiu <letchujiufly@outlook.com>
2024-04-16 18:58:01 +0800

    Home Login Register Navbar Footer Design

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder="Username" />
+        <input requiredtype="password" placeholder="Password" />
+        <button type="submit">Login</button>
+        <p>This is a error!</p>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+export default Login;
